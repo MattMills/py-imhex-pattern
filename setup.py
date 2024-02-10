5d0ffa0d4a4c356fa4cb46bc773a7457daa13611
Matt Mills <mmills@2bn.net>
2024-02-09 17:19:26 -0700

    Changes to setup.py for gcc

--- setup.py
+++ setup.py
@@ -12,9 +12,13 @@ extensions = [
             "external/PatternLanguage/lib/include/pl/", 
             "external/PatternLanguage/external/fmt/include/",
             "external/PatternLanguage/external/libwolv/libs/io/include",
+            "external/PatternLanguage/external/libwolv/libs/utils/include",
+            "external/PatternLanguage/external/libwolv/libs/containers/include",
+            "external/PatternLanguage/external/libwolv/libs/types/include",
             ],  
         language="c++",
-        extra_compile_args=["/std:c++20"],
+        extra_compile_args=["-std=c++23", '-Wno-stringop-overflow', '-Wno-dangling-reference',  '-fexceptions', '-frtti'], #windows: /std:c++20
+        extra_link_args=["-std=c++23"],
     ),
 ]
 
